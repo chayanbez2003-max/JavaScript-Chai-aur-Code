cb82e3bd50b6e2667663e04ad2402a454bbf8f62
chayanbez2003-max <chayanbez03@gmail.com>
2024-08-24 07:29:13 +0000

    container configuration done

---
+++ ControlFlow/switch.js
@@ -0,0 +1,16 @@
+const month ="march"
+
+switch(month){
+    case "jan":
+        console.log("January")
+        break;
+    case "feb":
+        console.log("January")
+        break;
+    case "march":
+        console.log("March")
+        break;
+    case "april":
+        console.log("January")
+        break;
+}
